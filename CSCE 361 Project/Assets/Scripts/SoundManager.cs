7ccc22a6faeb00a26a0cd9dd3e5a83b10b1fab3f
jnight444 <jaredthomasnightingale@gmail.com>
2017-10-21 22:27:39 -0500

    Created a sound manager & imported music

---
+++ CSCE 361 Project/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour {
+
+	public static SoundManager instance = null;
+	public AudioSource EFXSource;
+	public AudioSource musicSource;
+	public float lowPitchVariation = .9f;
+	public float highPitchVariation = 1.1f;
+
+
+	// Use this for initialization
+	void Start () {
+		if (instance == null) {
+			instance = this;
+		} else {
+			Destroy (gameObject);
+		}
+		DontDestroyOnLoad (gameObject);
+	}
+
+	public void playClip(AudioClip audioClip) {
+		EFXSource.clip = audioClip;
+
+		EFXSource.Play ();
+	}
+
+	public void randomPitchClip(AudioClip[] audioClips) {
+		EFXSource.pitch = (float)Random.Range (lowPitchVariation, highPitchVariation);
+
+		EFXSource.clip = audioClips[Random.Range(0, audioClips.Length)];
+
+		EFXSource.Play ();
+	}
+}

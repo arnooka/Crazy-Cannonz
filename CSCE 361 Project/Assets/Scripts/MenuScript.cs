9b0576fcde3762b478abf6be682897fedb440670
Grant <grant.tbird@gmail.com>
2017-10-22 05:17:23 -1000

    Main Menu push

---
+++ CSCE 361 Project/Assets/Scripts/MenuScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuScript : MonoBehaviour {
+
+	public Slider mainSlider;
+	public Text text;
+
+	// Use this for initialization
+	void Start () {
+
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+				
+		string x = mainSlider.value.ToString();
+		text.text = x;
+
+	}
+}

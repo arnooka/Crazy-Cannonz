4115f55c13db6538913226e2e4f9ed8c1106c293
Grant <grant.tbird@gmail.com>
2017-10-26 16:15:06 -0500

    Remove MatchSettings script
    Removed the MatchSettings script and placed all the old functionality in the MatchManager class to eliminate redundnancy.

--- CSCE 361 Project/Assets/Scripts/MatchManager.cs
+++ CSCE 361 Project/Assets/Scripts/MatchManager.cs
@@ -5,6 +5,9 @@ using UnityEngine.UI;
 
 public class MatchManager : MonoBehaviour {
 
+	public static double matchTime = 600.0, soundFXVolume = 1.0;
+	public static float musicVolume = 1.0f;
+
 	public Text timer;
 	public GameObject panel;
 
@@ -13,14 +16,16 @@ public class MatchManager : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 
-		theMatchMusic.volume = (float) MatchSettings.musicVolume;
+		theMatchMusic.volume = musicVolume;
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-		//timer.text = MatchSettings.matchTime.ToString();
+		matchTime -= Time.deltaTime;
+		//timer.text = matchTime.ToString();
+		print(matchTime);
 
 		//First check to see if the player wants to pause.
 		if (Input.GetKey (KeyCode.P)) {

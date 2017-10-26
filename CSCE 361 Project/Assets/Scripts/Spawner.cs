1a331f964a225a8ecc2fff01623b8fbaa35b0f66
Grant <grant.tbird@gmail.com>
2017-10-26 11:44:01 -0500

    Revert "Merge branch 'master' of https://github.com/nookavish/CSCE-361"
    This reverts commit dc9ea665ea8422bd31e8fd5f22d3d5bd6b44a195, reversing
    changes made to 8eb2c8b389766567060e2aa6824f800fded9d1c7.

--- CSCE 361 Project/Assets/Scripts/Spawner.cs
+++ CSCE 361 Project/Assets/Scripts/Spawner.cs
@@ -8,24 +8,23 @@ public class Spawner : MonoBehaviour
 	public GameObject[] whatToSpawnPrefab;
 	public GameObject[] whatToSpawnClone;
 
-	private int currentLocation;
-	private int crateType;
+	public int currentLocation;
 
 	public int maxTime = 8;
 	public int minTime = 2;
 	public int spawnTime;
 
-	private GameObject crate;
 
-	void Start() {
+	void Start()
+	{
 		Invoke("SpawnCrate", 3);
 	}
 		
-	void SpawnCrate() {
+	void SpawnCrate()
+	{
 		CancelInvoke ();
 		currentLocation = Random.Range (0, spawnLocations.Length);
-		crateType = Random.Range(0, whatToSpawnPrefab.Length);
-		whatToSpawnClone [currentLocation] = Instantiate (whatToSpawnPrefab [crateType], spawnLocations [currentLocation].transform.position, Quaternion.Euler (0, 0, 0));
+		whatToSpawnClone [currentLocation] = Instantiate (whatToSpawnPrefab [currentLocation], spawnLocations [currentLocation].transform.position, Quaternion.Euler (0, 0, 0));
 		Invoke ("SpawnCrate", Random.Range (minTime, maxTime));
 	}
 		

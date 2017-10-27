ebb85e67767f8dab0c1ddf74c4a017568e10710f
Grant <grant.tbird@gmail.com>
2017-10-27 05:02:46 -0500

    Fixing Menu...again

--- CSCE 361 Project/Assets/Scripts/MatchManager.cs
+++ CSCE 361 Project/Assets/Scripts/MatchManager.cs
@@ -2,13 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class MatchManager : MonoBehaviour {
 
 	public AudioSource theMatchMusic;
 	public GameObject panel;
 	public Text text;
-	public static double matchTime = 600.0, soundFXVolume = 1.0;
+	public static double matchTime = 600.0, soundFXVolume = 1.0, timeRemaining = 0.0;
+
+	public static int min = 0, sec = 0;
 
 	// Use this for initialization
 	void Start () {
@@ -17,8 +20,7 @@ public class MatchManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-
-<<<<<<< HEAD
+		
 		timeRemaining = matchTime - Time.timeSinceLevelLoad;
 
 		if (timeRemaining <= 0.0) {
@@ -29,10 +31,8 @@ public class MatchManager : MonoBehaviour {
 		sec = (int) timeRemaining % 60;
 
 		text.text = min.ToString () + ":" + sec.ToString ();
-=======
 		matchTime -= Time.deltaTime;
 		text.text = matchTime.ToString();
->>>>>>> 665cadd85cfc323df1b6c30606cce4e2e621524c
 
 		//First check to see if the player wants to pause.
 		if (Input.GetKey (KeyCode.P)) {

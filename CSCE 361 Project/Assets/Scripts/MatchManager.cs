ce3a9829e9250305a124cad38e785b23cbfdb160
Grant <grant.tbird@gmail.com>
2017-10-27 04:10:50 -0500

    More changes on the timer

--- CSCE 361 Project/Assets/Scripts/MatchManager.cs
+++ CSCE 361 Project/Assets/Scripts/MatchManager.cs
@@ -8,18 +8,22 @@ public class MatchManager : MonoBehaviour {
 	public AudioSource theMatchMusic;
 	public GameObject panel;
 	public Text text;
-	public static double matchTime = 600.0, soundFXVolume = 1.0;
+	public static double matchTime = 600.0, soundFXVolume = 1.0, timeRemaining = 0.0;
+	public static int min = 0, sec = 0;
 
 	// Use this for initialization
 	void Start () {
-
+		timeRemaining = matchTime;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-		matchTime -= Time.deltaTime;
-		text.text = matchTime.ToString();
+		timeRemaining = matchTime - Time.timeSinceLevelLoad; 
+		min = (int) timeRemaining / 60;
+		sec = (int) timeRemaining % 60;
+
+		text.text = min.ToString () + ":" + sec.ToString ();
 
 		//First check to see if the player wants to pause.
 		if (Input.GetKey (KeyCode.P)) {

b67a756e7d2bd185d2fc7929e4003a4c93898e71
Avinash Nooka <nookavish@gmail.com>
2017-10-27 01:16:03 -0500

    Y it do dis

--- CSCE 361 Project/Assets/Scripts/MatchManager.cs
+++ CSCE 361 Project/Assets/Scripts/MatchManager.cs
@@ -1,44 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
-using UnityEngine.SceneManagement;
 
 public class MatchManager : MonoBehaviour {
 
 	public AudioSource theMatchMusic;
-	public GameObject panel;
-	public Text text;
-	public static double matchTime = 10.0, soundFXVolume = 1.0, timeRemaining = 0.0;
-
-	public static int min = 0, sec = 0;
 
 	// Use this for initialization
 	void Start () {
 
+		//theMatchMusic.volume = (float) MatchSettings.musicVolume;
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-		timeRemaining = matchTime - Time.timeSinceLevelLoad;
 
-		if (timeRemaining <= 0.0) {
-			SceneManager.LoadScene ("PostMatch");
-		}
-
-		min = (int) timeRemaining / 60;
-		sec = (int) timeRemaining % 60;
-
-		text.text = min.ToString () + ":" + sec.ToString ();
-		matchTime -= Time.deltaTime;
-		text.text = matchTime.ToString();
-
+		//matchTime -= Time.deltaTime;
+		//timer.text = matchTime.ToString();
+		//print(matchTime);
+		//
 		//First check to see if the player wants to pause.
-		if (Input.GetKey (KeyCode.P)) {
-			panel.SetActive (false);
-			print ("p button pressed");
-		}
+		//if (Input.GetKey (KeyCode.P)) {
+		//	panel.SetActive (false);
+		//	print ("p button pressed");
+		//}
 		
 	}
 }

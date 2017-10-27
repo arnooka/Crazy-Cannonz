a50063db690c23fc2ff4f6e8ddbef112ddbc9e66
jnight444 <jaredthomasnightingale@gmail.com>
2017-10-26 22:13:48 -0500

    Did a BUNCH of stuff with sound in the menu and in game

--- CSCE 361 Project/Assets/Scripts/MenuScript.cs
+++ CSCE 361 Project/Assets/Scripts/MenuScript.cs
@@ -11,44 +11,68 @@ public class MenuScript : MonoBehaviour {
 	public Button optionsBtn, matchBtn, matchBackBtn, optionsBackBtn, matchStartBtn;
 	public Slider musicSlider, soundFXSlider;
 	public GameObject mainMenu, optionsMenu, matchMenu;
-	public AudioSource menuMusic;
+
 
 	// Use this for initialization
 	void Start () {
+        /// Get an instance of the sound manager, tell it that
+        /// we're in a menu, and play menu music
+        SoundManager soundManager = SoundManager.instance;
+        soundManager.inMenu = true;
+        soundManager.musicSource.Play();
 
+        /// Button for returning from match menu
 		matchBackBtn.onClick.AddListener (() => {
 			mainMenu.SetActive(true);
 			matchMenu.SetActive(false);
 		});
 
+        /// Button for returning from options menu
 		optionsBackBtn.onClick.AddListener (() => {
 			mainMenu.SetActive(true);
 			optionsMenu.SetActive(false);
 		});
 
+        /// Button for entering options menu
 		optionsBtn.onClick.AddListener (() => {
 			optionsMenu.SetActive(true);
 			mainMenu.SetActive(false);
 		});
 
+        /// Button for entering match menu
 		matchBtn.onClick.AddListener (() => {
 			matchMenu.SetActive (true);
 			mainMenu.SetActive (false);
 		});
 
+        /// Button for starting a match
 		matchStartBtn.onClick.AddListener (() => {
-			MatchSettings.matchTime = (double) matchTimerSlider.value;
-			matchMenu.SetActive(false);
+            //save time value in match settings
+			MatchSettings.matchTime = matchTimerSlider.value;
 
-			//This is only because menuMusic.Pause() and menuMusic.Stop() both seem to crash Unity for some reason...
-			menuMusic.volume = 0;
-			SceneManager.LoadScene ("Arena Mid");
+            /// Stop the menu music and tell the sound manager
+            /// that we're no longer at the menu
+            soundManager.musicSource.Stop();
+            soundManager.inMenu = false;
+
+            /// Hide the match menu
+			matchMenu.SetActive(false);
+			
+            /// Load the level scene
+            SceneManager.LoadScene ("Arena Mid");
 		});
 
 		musicSlider.onValueChanged.AddListener (delegate {
-			menuMusic.volume = musicSlider.value;
-			MatchSettings.musicVolume = (float) musicSlider.value;
+            //set music volume for sound manager and save
+            soundManager.musicSource.volume = musicSlider.value;
+			MatchSettings.musicVolume = musicSlider.value;
 		});
+
+        soundFXSlider.onValueChanged.AddListener(delegate {
+            //set sound effects volume for sound manager and save
+            soundManager.EFXSource.volume = soundFXSlider.value;
+            MatchSettings.soundFXVolume = soundFXSlider.value;
+        });
 
 	}
 
@@ -58,5 +82,4 @@ public class MenuScript : MonoBehaviour {
 		matchTimerText.text = matchTimerSlider.value.ToString();
 
 	}
-		
 }

5bf62f934177ee68ebab4513234458b9aa4c640e
Jake Field <jjfield3@gmail.com>
2017-11-29 18:12:15 -0600

    mapz
    bit yet to be done

---
+++ Code/CSCE 361 Project/Assets/Scripts/PlatformFloat.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformFloat : MonoBehaviour {
+
+	[SerializeField]
+	private float amplitude = 1.2f;
+	[SerializeField]
+	private float frequency = 0.3f;
+
+	private Vector2 posOffset = new Vector2 ();
+	private Vector2 tempPos = new Vector2 ();
+
+	void Start () {
+		posOffset = transform.position;
+	}
+
+	void Update () {
+
+		tempPos = posOffset;
+		tempPos.y += Mathf.Sin (Time.fixedTime * Mathf.PI * frequency) * amplitude;
+
+		transform.position = tempPos;
+	}
+
+}
